3f8fa09df964f12840abdf13fdebd7edc3ed496c
Nicky <nickyprabowo78@gmail.com>
2022-11-04 15:44:46 +0700

    feat(fish): add fish entity for type checking

--- src/domain/fish/api/index.ts
+++ src/domain/fish/api/index.ts
@@ -1,11 +1,9 @@
 import { http } from "common/utility/http-client"
+import { Commodities } from "../entity";
+
 const ALL_COMMODITIES_API = `${process.env.REACT_APP_COMMODITY_LIST}`;
 
-export const getCommodities = async () => {
-  try {
+export const getCommodities = async (): Promise<Commodities> => {
     const response = await http.get(ALL_COMMODITIES_API);
     return response.data
-  } catch (error) {
-    return error;
-  }
 };
---
+++ src/domain/fish/entity/index.ts
@@ -0,0 +1,25 @@
+export interface Fish {
+  uuid: string,
+  komoditas: string,
+  area_provinsi: string,
+  area_kota: string,
+  size: number,
+  price: number,
+  tgl_parsed: string,
+  timestamp: string
+}
+
+export interface Area {
+  province: string,
+  city: string
+}
+
+export interface Size {
+  size: number
+}
+
+export type OptionArea = Area[]
+
+export type OptionSize = Size[]
+
+export type Commodities = Fish[]
