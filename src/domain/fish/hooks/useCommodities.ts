27daa0a76e1d0f812dcf3e17af5741a18d07cfec
Nicky <nickyprabowo78@gmail.com>
2022-11-05 19:20:47 +0700

    feat(fish): add success callback to useCommodities

--- src/domain/fish/hooks/useCommodities.ts
+++ src/domain/fish/hooks/useCommodities.ts
@@ -1,8 +1,17 @@
 import { useQuery } from "common/utility/request"
+import { Fishes } from "../entity";
 import { getCommodities } from "../api"
 
-const useCommodities = () => {
-  return useQuery(["commodities"], getCommodities)
+interface IUseCommodities {
+  successCb: (data: Fishes) => void
+}
+
+const useCommodities = (config: IUseCommodities = { successCb: () => {} }) => {
+  return useQuery(["commodities"], getCommodities, {
+    onSuccess(data) {
+      config.successCb(data)
+    },
+  })
 }
 
 export default useCommodities;
