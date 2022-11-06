14d7993743bf1015eb96d4bc587b21a07668546d
Nicky <nickyprabowo78@gmail.com>
2022-11-06 22:10:15 +0700

    chore(hooks): fix useCommodities hook

--- src/domain/fish/hooks/useCommodities.ts
+++ src/domain/fish/hooks/useCommodities.ts
@@ -2,15 +2,16 @@ import { useQuery } from "common/utility/request"
 import { getCommodities } from "../api"
 
 import { Fishes } from "../entity";
-import { FilterDto } from "../dto";
+import { FilterDto, PaginationDto } from "../dto";
 
 interface IUseCommodities {
+  pagination: PaginationDto,
   filter?: Partial<FilterDto>,
   successCb?: (data: Fishes) => void
 }
 
-const useCommodities = (config?: IUseCommodities) => {
-  return useQuery(["commodities", config?.filter], () => getCommodities(config?.filter), {
+const useCommodities = (config: IUseCommodities) => {
+  return useQuery(["commodities", config.pagination, config?.filter], () => getCommodities(config.pagination, config?.filter), {
     onSuccess(data) {
       if(typeof config?.successCb !== "undefined") config.successCb(data)
     },
