650cf73ef100795415ad51c72b4b9864a2d4de0b
Nicky <nickyprabowo78@gmail.com>
2022-11-06 22:27:44 +0700

    feat(request): turn off refetch on window focus

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { QueryClientProvider, QueryClient } from 'common/utility/request';
 import Commodities  from 'domain/fish/container/Commodities';
 import "./App.scss"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 function App() {
   return (
