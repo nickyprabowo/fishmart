f9700503132abf993db5e86c212107541ec1b3e4
Nicky <nickyprabowo78@gmail.com>
2022-11-06 14:12:28 +0700

    feat(api): use URL API to create url

--- src/domain/fish/api/index.ts
+++ src/domain/fish/api/index.ts
@@ -3,14 +3,19 @@ import { isEmpty } from "common/utility/object"
 import { Fishes, OptionArea, OptionSize } from "../entity";
 import { FilterDto } from "../dto";
 
+const BASE_URL = process.env.REACT_APP_COMMODITY_API_BASE_URL
 const ALL_COMMODITIES_API = `${process.env.REACT_APP_COMMODITY_LIST}`;
 const OPTION_SIZE_API = `${process.env.REACT_APP_COMMODITY_OPTION_SIZE}`;
 const OPTION_AREA_API = `${process.env.REACT_APP_COMMODITY_OPTION_AREA}`;
 
 export const getCommodities = async (query?: Partial<FilterDto>): Promise<Fishes> => {
-    const paramJSON = isEmpty(query) ? "?search=".concat(JSON.stringify(query)) : ""
-    const URL = `${ALL_COMMODITIES_API}${paramJSON}`
-    const response = await http.get(URL);
+    let url = new window.URL(ALL_COMMODITIES_API, BASE_URL)
+    if(!isEmpty(query)){
+        let params = new URLSearchParams({search: JSON.stringify(query)});
+        url.search = params.toString()
+    }
+
+    const response = await http.get(url.href);
     return response.data
 };
 
